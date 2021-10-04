e5e4d7425a9838b1a1acf3cf24d6a43a8d4613cf
dexiion <otonielcuentas@gmail.com>
2021-10-04 16:39:56 +0100

    refactor: extract method

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -7,10 +7,16 @@
             if (string.IsNullOrEmpty(input)) return 0;
             if(input.Contains(SEPARATOR))
             {
-                var splittedInput = input.Split(SEPARATOR);
-                return int.Parse(splittedInput[0]) + int.Parse(splittedInput[1]);
+                var transformedInput = Transform(input);
+                return transformedInput[0] + transformedInput[1];
             }
             return int.Parse(input);
+        }
+
+        private static int[] Transform(string input)
+        {
+            var splittedInput = input.Split(SEPARATOR);
+            return new int[] { int.Parse(splittedInput[0]), int.Parse(splittedInput[1]) };
         }
     }
 }
