7713afdb5fcf4aa63378f0916149a2461129267a
dexiion <otonielcuentas@gmail.com>
2021-10-07 09:52:52 +0100

    green: return_addition_when_separator_is_new_line

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -4,21 +4,25 @@ using System.Linq;
 namespace StringCalculator {
     public static class StringCalculator
     {
-        private const string SEPARATOR = ",";
+        private const string SEPARATOR_COMA = ",";
+        private const string SEPARATOR_NEW_LINE = "\n";
         public static int Add(string input)
         {
             if (string.IsNullOrEmpty(input)) return 0;
-            if(input.Contains(SEPARATOR))
+            if(input.Contains(SEPARATOR_COMA) || input.Contains(SEPARATOR_NEW_LINE))
             {
                 var transformedInput = Transform(input);
+
                 return transformedInput.Sum();
             }
+            
             return int.Parse(input);
         }
 
         private static IEnumerable<int> Transform(string input)
         {
-            return input.Split(SEPARATOR).Select(int.Parse);
+            var replace = input.Replace(SEPARATOR_NEW_LINE, SEPARATOR_COMA);
+            return replace.Split(SEPARATOR_COMA).Select(int.Parse);
         }
     }
 }
