b0a3f441e48ca84862f3c74f68b497eb1a524370
dexiion <otonielcuentas@gmail.com>
2021-10-07 11:01:38 +0100

    green: return_addition_when_separator_is_defined_by_input

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,22 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace StringCalculator {
+namespace StringCalculator
+{
     public static class StringCalculator
     {
-        private const string SEPARATOR_COMA = ",";
-        private const string SEPARATOR_NEW_LINE = "\n";
+        private const char SEPARATOR_COMMA = ',';
+        private const char SEPARATOR_NEW_LINE = '\n';
+        private const string DELIMITER_SELECTOR = "//";
+
         public static int Add(string input)
         {
-            if (string.IsNullOrEmpty(input)) return 0;
-            if (!input.Contains(SEPARATOR_COMA) && !input.Contains(SEPARATOR_NEW_LINE)) return int.Parse(input);
+            if (string.IsNullOrEmpty(input))
+                return 0;
+            if (input.Contains(DELIMITER_SELECTOR))
+                input = ReplaceDelimiter(input);
             return Transform(input).Sum();
+
         }
 
         private static IEnumerable<int> Transform(string input)
         {
-            input = input.Replace(SEPARATOR_NEW_LINE, SEPARATOR_COMA);
-            return input.Split(SEPARATOR_COMA).Select(int.Parse);
+            input = input.Replace(SEPARATOR_NEW_LINE, SEPARATOR_COMMA);
+            return input.Split(SEPARATOR_COMMA).Select(int.Parse);
         }
+
+        private static string ReplaceDelimiter(string input)
+        {
+            var newDelimiter = input[2];
+            input = input.Substring(4);
+            return input.Replace(newDelimiter, SEPARATOR_COMMA);
+        }
+
     }
 }
